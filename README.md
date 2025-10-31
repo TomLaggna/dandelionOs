a3241d52ec1288f51ea016d1dbebc98c1bb8e95d
Thomas Lackner <tlackner@student.ethz.ch>
2025-10-31 11:07:00 +0100

    server migration: slight cleanup; adjust readme, remove commented code that has been migrated

--- README.md
+++ README.md
@@ -116,29 +116,20 @@ If you use Dandelion, please cite our paper:
 For testing the C code to interact with Cheri we are using unity which is included directly in the project.
 
 # Unikraft (IN PROGRESS)
-To try a Unikraft build of Dandelion, use
-```
-KRAFTKIT_TARGET=dandelion-os cargo +nightly build -Z build-std=std,panic_abort --target x86_64-unikraft-linux-musl --features=mmu,reqwest_io
-```
-You can make sure first that the native build works via 
-```
-cargo build --bin dandelion_server --features=mmu,reqwest_io
-```
-
-## Current state
 Migration to Unikraft is in Progress, only parts of dandelion are included in the build. To make a unikraft build, use
 ```
 KRAFTKIT_TARGET=dandelion-os cargo +nightly build -Z build-std=std,panic_abort --target x86_64-unikraft-linux-musl --bin dandelion_server
 ```
-__note on instability__: 
+## note on build instability 
 - when there are multiple binary targets enabled, kraft seems to have issues with parallelism; you have to run the build command multiple times until it works. To ensure progress, you can pass in `-j 1` to cargo s.t. at least one member is built.
 - incremental builds seem to cause issues. You can keep the target directory to keep the rust part of building. However to update an existing unikraft build, delete the .unikraft directory and the generated .config.dandelion-os_qemu-x86_64 (sometimes you also need to delete the target directory, not sure why)
 
 Run the built image by `kraft run --rm --plat qemu --arch x86_64 -p 3000:3000 .`
 During migration, a webserver is included, try reaching it via `curl localhost:3000`
 
-__multicore__: Currently, multicore __does not work__.
-If you need more than 1 core, you need to run qemu directly instead of kraft run:
+## Multicore
+Currently, multicore __does not work__.
+Still, if you need to start the kernel with more than 1 core available, use:
 ```
 qemu-system-x86_64 \
 -kernel .unikraft/build/dandelion-os_qemu-x86_64 \
@@ -153,7 +144,8 @@ qemu-system-x86_64 \
 
 Use CTRL-a+x to stop.
 
-__debugging__ following the tutorial https://unikraft.org/docs/internals/debugging and slightly adjusting the commands:
+## Debugging
+following the tutorial https://unikraft.org/docs/internals/debugging and slightly adjusting the commands:
 - Run the non-debug built binary directly on qemu/KVM (and freeze startup using -S): `qemu-system-x86_64 -s -S -cpu host -enable-kvm -m 128 -nodefaults -no-acpi -display none -serial stdio -device isa-debug-exit -kernel .unikraft/build/dandelion-os_qemu-x86_64 -device virtio-net-pci,mac=02:b0:b0:d3:d2:01,netdev=hostnet0 -netdev user,id=hostnet0,hostfwd=tcp::3000-:3000`
 - Attach gdb at port 1234 (exposed by -s option in qemu), giving the debug build as a parameter `gdb --eval-command="target remote :1234" .unikraft/build/dandelion-os_qemu-x86_64.dbg`
 - register a hardware breakpoint via e.g.: `hbreak main`
--- server/src/main.rs
+++ server/src/main.rs
@@ -322,41 +322,6 @@ enum DispatcherCommand {
 //     return Ok::<_, Infallible>(response);
 // }
 
-// async fn service(
-//     req: Request<Incoming>,
-//     dispatcher: mpsc::Sender<DispatcherCommand>,
-// ) -> Result<Response<DandelionBody>, Infallible> {
-//     let uri = req.uri().path();
-//     match uri {
-//         // TODO rename to cold func and hot func, remove matmul, compute, io
-//         "/register/function" => register_function(req, dispatcher).await,
-//         "/register/composition" => register_composition(req, dispatcher).await,
-//         "/cold/matmul"
-//         | "/cold/matmulstore"
-//         | "/cold/compute"
-//         | "/cold/io"
-//         | "/cold/chain_scaling"
-//         | "/cold/middleware_app"
-//         | "/cold/compression_app"
-//         | "/cold/python_app" => serve_request(true, req, dispatcher).await,
-//         "/hot/matmul"
-//         | "/hot/matmulstore"
-//         | "/hot/compute"
-//         | "/hot/io"
-//         | "/hot/chain_scaling"
-//         | "/hot/middleware_app"
-//         | "/hot/compression_app"
-//         | "/hot/python_app" => serve_request(false, req, dispatcher).await,
-//         "/stats" => serve_stats(req).await,
-//         other_uri => {
-//             trace!("Received request on {}", other_uri);
-//             Ok::<_, Infallible>(Response::new(DandelionBody::from_vec(
-//                 format!("Hello, Wor\n").into_bytes(),
-//             )))
-//         }
-//     }
-// }
-
 /// Recording setup
 static TRACING_ARCHIVE: OnceLock<Archive> = OnceLock::new();
 
@@ -426,247 +391,72 @@ async fn dispatcher_loop(
     }
 }
 
-// async fn service_loop(request_sender: mpsc::Sender<DispatcherCommand>, port: u16) {
-//     // socket to listen to
-//     let addr: SocketAddr = SocketAddr::from(([0, 0, 0, 0], port));
-//     let listener = TcpListener::bind(addr).await.unwrap();
-//     // signal handlers for gracefull shutdown
-//     let mut sigterm_stream = tokio::signal::unix::signal(SignalKind::terminate()).unwrap();
-//     let mut sigint_stream = tokio::signal::unix::signal(SignalKind::interrupt()).unwrap();
-//     let mut sigquit_stream = tokio::signal::unix::signal(SignalKind::quit()).unwrap();
-//     loop {
-//         tokio::select! {
-//             connection_pair = listener.accept() => {
-//                 let (stream,_) = connection_pair.unwrap();
-//                 let loop_dispatcher = request_sender.clone();
-//                 let io = hyper_util::rt::TokioIo::new(stream);
-//                 tokio::task::spawn(async move {
-//                     let service_dispatcher_ptr = loop_dispatcher.clone();
-//                     if let Err(err) = hyper_util::server::conn::auto::Builder::new(hyper_util::rt::TokioExecutor::new())
-//                         .serve_connection_with_upgrades(
-//                             io,
-//                             service_fn(|req| service(req, service_dispatcher_ptr.clone())),
-//                         )
-//                         .await
-//                     {
-//                         error!("Request serving failed with error: {:?}", err);
-//                     }
-//                 });
-//             }
-//             _ = sigterm_stream.recv() => return,
-//             _ = sigint_stream.recv() => return,
-//             _ = sigquit_stream.recv() => return,
-//         }
-//     }
-// }
+async fn service_loop(request_sender: mpsc::Sender<DispatcherCommand>, port: u16) {
+    // socket to listen to
+    let addr: SocketAddr = SocketAddr::from(([0, 0, 0, 0], port));
+    let listener = TcpListener::bind(addr).await.unwrap();
+    // signal handlers for gracefull shutdown
+    let mut sigterm_stream = tokio::signal::unix::signal(SignalKind::terminate()).unwrap();
+    let mut sigint_stream = tokio::signal::unix::signal(SignalKind::interrupt()).unwrap();
+    let mut sigquit_stream = tokio::signal::unix::signal(SignalKind::quit()).unwrap();
+    loop {
+        tokio::select! {
+            connection_pair = listener.accept() => {
+                let (stream,_) = connection_pair.unwrap();
+                let loop_dispatcher = request_sender.clone();
+                let io = hyper_util::rt::TokioIo::new(stream);
+                tokio::task::spawn(async move {
+                    let service_dispatcher_ptr = loop_dispatcher.clone();
+                    if let Err(err) = hyper_util::server::conn::auto::Builder::new(hyper_util::rt::TokioExecutor::new())
+                        .serve_connection_with_upgrades(
+                            io,
+                            service_fn(|req| service(req, service_dispatcher_ptr.clone())),
+                        )
+                        .await
+                    {
+                        error!("Request serving failed with error: {:?}", err);
+                    }
+                });
+            }
+            _ = sigterm_stream.recv() => return,
+            _ = sigint_stream.recv() => return,
+            _ = sigquit_stream.recv() => return,
+        }
+    }
+}
+async fn service(
+    req: Request<Incoming>,
+    dispatcher: mpsc::Sender<DispatcherCommand>,
+) -> Result<Response<String>, Infallible> {
+    let uri = req.uri().path();
+    match uri {
+        // TODO rename to cold func and hot func, remove matmul, compute, io
+        // "/register/function" => register_function(req, dispatcher).await,
+        // "/register/composition" => register_composition(req, dispatcher).await,
+        // "/cold/matmul"
+        // | "/cold/matmulstore"
+        // | "/cold/compute"
+        // | "/cold/io"
+        // | "/cold/chain_scaling"
+        // | "/cold/middleware_app"
+        // | "/cold/compression_app"
+        // | "/cold/python_app" => serve_request(true, req, dispatcher).await,
+        // "/hot/matmul"
+        // | "/hot/matmulstore"
+        // | "/hot/compute"
+        // | "/hot/io"
+        // | "/hot/chain_scaling"
+        // | "/hot/middleware_app"
+        // | "/hot/compression_app"
+        // | "/hot/python_app" => serve_request(false, req, dispatcher).await,
+        // "/stats" => serve_stats(req).await,
+        other_uri => {
+            trace!("Received request on {}", other_uri);
+            Ok::<_, Infallible>(Response::new("Hello World!".to_string()))
+        }
+    }
+}
 
-// fn main() -> () {
-//     // check if there is a configuration file
-//     let config = dandelion_server::config::DandelionConfig::get_config();
-
-//     println!("config: {:?}", config);
-
-//     let default_warn_level = if cfg!(debug_assertions) {
-//         "debug"
-//     } else {
-//         "warn"
-//     };
-
-//     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(default_warn_level))
-//         .init();
-
-//     // Initilize metric collection
-//     match TRACING_ARCHIVE.set(Archive::init()) {
-//         Ok(_) => (),
-//         Err(_) => panic!("Failed to initialize tracing archive"),
-//     }
-
-//     // find available resources
-//     let num_phyiscal_cores = u8::try_from(num_cpus::get_physical()).unwrap();
-//     let num_virt_cores = u8::try_from(num_cpus::get()).unwrap();
-//     if num_phyiscal_cores != num_virt_cores {
-//         warn!(
-//             "Hyperthreading might be enabled detected {} logical and {} physical cores",
-//             num_virt_cores, num_phyiscal_cores
-//         );
-//     }
-
-//     let resource_conversion = |core_index| ComputeResource::CPU(core_index);
-
-//     let dispatcher_cores = config.get_dispatcher_cores();
-//     let frontend_cores = config.get_frontend_cores();
-//     let communication_cores = config
-//         .get_communication_cores()
-//         .into_iter()
-//         .map(|core| resource_conversion(core))
-//         .collect();
-//     let compute_cores = config
-//         .get_computation_cores()
-//         .into_iter()
-//         .map(|core| resource_conversion(core))
-//         .collect();
-
-//     println!("core allocation:");
-//     println!("frontend cores {:?}", frontend_cores);
-//     println!("dispatcher cores: {:?}", dispatcher_cores);
-//     println!("communication cores: {:?}", communication_cores);
-//     println!("compute cores: {:?}", compute_cores);
-
-//     // make multithreaded front end runtime
-//     // set up tokio runtime, need io in any case
-//     let mut runtime_builder = Builder::new_multi_thread();
-//     runtime_builder.enable_io();
-//     runtime_builder.worker_threads(frontend_cores.len());
-//     runtime_builder.on_thread_start(move || {
-//         static ATOMIC_INDEX: AtomicUsize = AtomicUsize::new(0);
-//         let core_index = ATOMIC_INDEX.fetch_add(1, Ordering::SeqCst);
-//         if !core_affinity::set_for_current(CoreId {
-//             id: frontend_cores[core_index].into(),
-//         }) {
-//             return;
-//         }
-//         info!(
-//             "Frontend thread running on core {}",
-//             frontend_cores[core_index]
-//         );
-//     });
-//     runtime_builder.global_queue_interval(10);
-//     runtime_builder.event_interval(10);
-//     let runtime = runtime_builder.build().unwrap();
-
-//     let dispatcher_runtime = Builder::new_multi_thread()
-//         .worker_threads(dispatcher_cores.len())
-//         .on_thread_start(move || {
-//             static ATOMIC_INDEX: AtomicUsize = AtomicUsize::new(0);
-//             let core_index = ATOMIC_INDEX.fetch_add(1, Ordering::SeqCst);
-//             if !core_affinity::set_for_current(CoreId {
-//                 id: dispatcher_cores[core_index].into(),
-//             }) {
-//                 return;
-//             }
-//             info!(
-//                 "Dispatcher thread running on core {}",
-//                 dispatcher_cores[core_index]
-//             );
-//         })
-//         .build()
-//         .unwrap();
-//     let (dispatcher_sender, dispatcher_recevier) = mpsc::channel(1000);
-
-//     // set up dispatcher configuration basics
-//     let mut pool_map = BTreeMap::new();
-
-//     // insert engines for the currentyl selected compute engine type
-//     // todo add function to machine config to detect resources and auto generate this
-//     #[cfg(feature = "wasm")]
-//     let engine_type = EngineType::RWasm;
-//     #[cfg(feature = "mmu")]
-//     let engine_type = EngineType::Process;
-//     #[cfg(feature = "kvm")]
-//     let engine_type = EngineType::Kvm;
-//     #[cfg(feature = "cheri")]
-//     let engine_type = EngineType::Cheri;
-//     #[cfg(any(feature = "cheri", feature = "wasm", feature = "mmu", feature = "kvm"))]
-//     pool_map.insert(engine_type, compute_cores);
-//     #[cfg(feature = "reqwest_io")]
-//     pool_map.insert(EngineType::Reqwest, communication_cores);
-//     let resource_pool = ResourcePool {
-//         engine_pool: futures::lock::Mutex::new(pool_map),
-//     };
-
-//     // get RAM size
-//     // TODO could be a configuration, open question on how to split between engines
-//     // or if we unify somehow and have one underlying pool
-//     let max_ram = read_to_string("/proc/meminfo")
-//         .unwrap()
-//         .lines()
-//         .find_map(|line| {
-//             line.strip_prefix("MemTotal:")
-//                 .and_then(|line| line.strip_suffix("kB"))
-//                 .and_then(|line| Some(line.trim().parse::<usize>()))
-//         })
-//         .unwrap()
-//         .unwrap()
-//         * 1024;
-
-//     let memory_pool = BTreeMap::from([
-//         (
-//             DomainType::Mmap,
-//             MemoryResource::Anonymous { size: max_ram },
-//         ),
-//         #[cfg(feature = "cheri")]
-//         (
-//             DomainType::Cheri,
-//             MemoryResource::Anonymous { size: max_ram },
-//         ),
-//         #[cfg(feature = "mmu")]
-//         (
-//             DomainType::Process,
-//             MemoryResource::Shared {
-//                 id: 0,
-//                 size: max_ram,
-//             },
-//         ),
-//         #[cfg(feature = "wasm")]
-//         (
-//             DomainType::RWasm,
-//             MemoryResource::Anonymous { size: max_ram },
-//         ),
-//     ]);
-
-//     // Create an ARC pointer to the dispatcher for thread-safe access
-//     let dispatcher = Box::leak(Box::new(
-//         Dispatcher::init(resource_pool, memory_pool).expect("Should be able to start dispatcher"),
-//     ));
-//     // start dispatcher
-//     dispatcher_runtime.spawn(dispatcher_loop(dispatcher_recevier, dispatcher));
-
-//     let _guard = runtime.enter();
-
-//     // TODO would be nice to just print server ready with all enabled features if that would be possible
-//     print!("Server start with features:");
-//     #[cfg(feature = "cheri")]
-//     print!(" cheri");
-//     #[cfg(feature = "mmu")]
-//     print!(" mmu");
-//     #[cfg(feature = "kvm")]
-//     print!(" kvm");
-//     #[cfg(feature = "wasm")]
-//     print!(" wasm");
-//     #[cfg(feature = "reqwest_io")]
-//     print!(" request_io");
-//     #[cfg(feature = "timestamp")]
-//     print!(" timestamp");
-//     print!("\n");
-
-//     // Run this server for... forever... unless I receive a signal!
-//     runtime.block_on(service_loop(dispatcher_sender, config.port));
-
-//     // clean up folder in tmp that is used for function storage
-//     let removal_error = std::fs::remove_dir_all(FUNCTION_FOLDER_PATH);
-//     if let Err(err) = removal_error {
-//         warn!("Removing function folder failed with: {}", err);
-//     }
-//     // clean up folder with shared files in case the context backed by shared files left some behind
-//     for shm_dir_entry in std::fs::read_dir("/dev/shm/").unwrap() {
-//         if let Ok(shm_file) = shm_dir_entry {
-//             if shm_file
-//                 .file_name()
-//                 .into_string()
-//                 .unwrap()
-//                 .starts_with("shm_")
-//             {
-//                 warn!(
-//                     "Found left over shared memory file: {:?}",
-//                     shm_file.file_name()
-//                 );
-//                 if std::fs::remove_file(shm_file.path()).is_err() {
-//                     warn!("Failed to remove shared memory file {:?}", shm_file.path());
-//                 }
-//             }
-//         }
-//     }
-// }
 #[tokio::main]
 async fn main() {
     let default_warn_level = if cfg!(debug_assertions) {
@@ -826,70 +616,4 @@ async fn main() {
 
     // Run this server for... forever... unless I receive a signal!
     tokio::spawn(service_loop(dispatcher_sender, config.port)).await;
-}
-
-async fn service_loop(request_sender: mpsc::Sender<DispatcherCommand>, port: u16) {
-    // socket to listen to
-    let addr: SocketAddr = SocketAddr::from(([0, 0, 0, 0], port));
-    let listener = TcpListener::bind(addr).await.unwrap();
-    // signal handlers for gracefull shutdown
-    let mut sigterm_stream = tokio::signal::unix::signal(SignalKind::terminate()).unwrap();
-    let mut sigint_stream = tokio::signal::unix::signal(SignalKind::interrupt()).unwrap();
-    let mut sigquit_stream = tokio::signal::unix::signal(SignalKind::quit()).unwrap();
-    loop {
-        tokio::select! {
-            connection_pair = listener.accept() => {
-                let (stream,_) = connection_pair.unwrap();
-                let loop_dispatcher = request_sender.clone();
-                let io = hyper_util::rt::TokioIo::new(stream);
-                tokio::task::spawn(async move {
-                    let service_dispatcher_ptr = loop_dispatcher.clone();
-                    if let Err(err) = hyper_util::server::conn::auto::Builder::new(hyper_util::rt::TokioExecutor::new())
-                        .serve_connection_with_upgrades(
-                            io,
-                            service_fn(|req| service(req, service_dispatcher_ptr.clone())),
-                        )
-                        .await
-                    {
-                        error!("Request serving failed with error: {:?}", err);
-                    }
-                });
-            }
-            _ = sigterm_stream.recv() => return,
-            _ = sigint_stream.recv() => return,
-            _ = sigquit_stream.recv() => return,
-        }
-    }
-}
-async fn service(
-    req: Request<Incoming>,
-    dispatcher: mpsc::Sender<DispatcherCommand>,
-) -> Result<Response<String>, Infallible> {
-    let uri = req.uri().path();
-    match uri {
-        // TODO rename to cold func and hot func, remove matmul, compute, io
-        // "/register/function" => register_function(req, dispatcher).await,
-        // "/register/composition" => register_composition(req, dispatcher).await,
-        // "/cold/matmul"
-        // | "/cold/matmulstore"
-        // | "/cold/compute"
-        // | "/cold/io"
-        // | "/cold/chain_scaling"
-        // | "/cold/middleware_app"
-        // | "/cold/compression_app"
-        // | "/cold/python_app" => serve_request(true, req, dispatcher).await,
-        // "/hot/matmul"
-        // | "/hot/matmulstore"
-        // | "/hot/compute"
-        // | "/hot/io"
-        // | "/hot/chain_scaling"
-        // | "/hot/middleware_app"
-        // | "/hot/compression_app"
-        // | "/hot/python_app" => serve_request(false, req, dispatcher).await,
-        // "/stats" => serve_stats(req).await,
-        other_uri => {
-            trace!("Received request on {}", other_uri);
-            Ok::<_, Infallible>(Response::new("Hello World!".to_string()))
-        }
-    }
 }
